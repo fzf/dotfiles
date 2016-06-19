d0db9050d75fd4abb0c49f6039ff7b013a80421d
Darren Cheng <darren@thanx.com>
2016-06-19 13:14:01 -0700

    Disabling swipe.

--- install/osx.sh
+++ install/osx.sh
@@ -22,9 +22,9 @@ echo "Configuring system key press speeds"
 defaults write -g ApplePressAndHoldEnabled -bool false
 # set key repeat rates
 defaults write -g InitialKeyRepeat -int 12
-defaults write -g KeyRepeat -int 3
+defaults write -g KeyRepeat -int 10
 
-echo "Disabling natural scrolling"
+echo "Enable natural scrolling"
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
 
 echo "Enabling Finder status bar"
@@ -39,155 +39,159 @@ defaults write com.apple.notificationcenterui bannerTime 2.5
 echo "Disabling autocorrect"
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
-echo "Disable natural scroll"
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
 echo "Disable opening and closing window animations"
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-# Expand save panel by default
+echo "Expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-# Expand print panel by default
+echo "Expand print panel by default"
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 
-# Automatically quit printer app once the print jobs complete
+echo "Automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# Disable the “Are you sure you want to open this application?” dialog
+echo "Disable the “Are you sure you want to open this application?” dialog"
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Disable smart quotes as they’re annoying when typing code
+echo "Disable smart quotes as they’re annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-# Disable smart dashes as they’re annoying when typing code
+echo "Disable smart dashes as they’re annoying when typing code"
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-# Hibernation for SSD and max filevault security
+echo "Hibernation for SSD and max filevault security"
 sudo pmset -a destroyfvkeyonstandby 1 hibernatemode 25
 
-# Disable the sudden motion sensor as it’s not useful for SSDs
+echo "Disable the sudden motion sensor as it’s not useful for SSDs"
 sudo pmset -a sms 0
 
-# Trackpad: enable tap to click for this user and for the login screen
+echo "Trackpad: enable tap to click for this user and for the login screen"
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# Disable “natural” (Lion-style) scrolling
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+echo "Trackpad: disable swipe"
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerHorizSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.fourFingerHorizSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerVertSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.fourFingerVertSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture -int 0
 
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
+echo "Enable full keyboard access for all controls"
+echo "(e.g. enable Tab in modal dialogs)"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Set a blazingly fast keyboard repeat rate
+echo "Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Disable auto-correct
+echo "Disable auto-correct"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# Require password immediately after sleep or screen saver begins
+echo "Require password immediately after sleep or screen saver begins"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Finder: disable window animations and Get Info animations
+echo "Finder: disable window animations and Get Info animations"
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-# Show icons for hard drives, servers, and removable media on the desktop
+echo "Show icons for hard drives, servers, and removable media on the desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Finder: show hidden files by default
+echo "Finder: show hidden files by default"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Finder: show all filename extensions
+echo "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show all filename extensions
+echo "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show status bar
+echo "Finder: show status bar"
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: show path bar
+echo "Finder: show path bar"
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Finder: allow text selection in Quick Look
+echo "Finder: allow text selection in Quick Look"
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-# Display full POSIX path as Finder window title
+echo "Display full POSIX path as Finder window title"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# When performing a search, search the current folder by default
+echo "When performing a search, search the current folder by default"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Disable the warning when changing a file extension
+echo "Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Enable spring loading for directories
+echo "Enable spring loading for directories"
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 
-# Remove the spring loading delay for directories
+echo "Remove the spring loading delay for directories"
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
-# Avoid creating .DS_Store files on network volumes
+echo "Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+echo "Use list view in all Finder windows by default"
+echo "Four-letter codes for the other view modes: icnv, clmv, Flwv"
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Disable the warning before emptying the Trash
+echo "Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-# Empty Trash securely by default
+echo "Empty Trash securely by default"
 defaults write com.apple.finder EmptyTrashSecurely -bool true
 
-# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+echo "Enable AirDrop over Ethernet and on unsupported Macs running Lion"
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
-# Show the ~/Library folder
+echo "Show the ~/Library folder"
 chflags nohidden ~/Library
 
-# Enable spring loading for all Dock items
+echo "Enable spring loading for all Dock items"
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-# Speed up Mission Control animations
+echo "Speed up Mission Control animations"
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-# Disable Dashboard
+echo "Disable Dashboard"
 defaults write com.apple.dashboard mcx-disabled -bool true
 
-# Don’t show Dashboard as a Space
+echo "Don’t show Dashboard as a Space"
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
-# Remove the auto-hiding Dock delay
+echo "Remove the auto-hiding Dock delay"
 defaults write com.apple.dock autohide-delay -float 0
-# Remove the animation when hiding/showing the Dock
+echo "Remove the animation when hiding/showing the Dock"
 defaults write com.apple.dock autohide-time-modifier -float 0
 
-# Automatically hide and show the Dock
+echo "Automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool true
 
-# Make Dock icons of hidden applications translucent
+echo "Make Dock icons of hidden applications translucent"
 defaults write com.apple.dock showhidden -bool true
 
-# Reset Launchpad
+echo "Reset Launchpad"
 find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
 
-# Only use UTF-8 in Terminal.app
+echo "Only use UTF-8 in Terminal.app"
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Don’t display the annoying prompt when quitting iTerm
+echo "Don’t display the annoying prompt when quitting iTerm"
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-# Prevent Time Machine from prompting to use new hard drives as backup volume
+echo "Prevent Time Machine from prompting to use new hard drives as backup volume"
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# ensure changes take effect immediately
+echo "ensure changes take effect immediately"
 killall Dock
 killall Finder
 killall SystemUIServer
