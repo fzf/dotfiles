88fce230e19bac60f468d962edacdc684aae981e
Fletcher Fowler <fletch@fzf.me>
2017-03-01 18:16:52 -0800

    Adding CCMenu.

--- install/mas.sh
+++ install/mas.sh
@@ -1,10 +1,12 @@
 # 1Password
 mas install 443987910
+# CCMenu
+mas install 603117688
 # Spark
 mas install 1176895641
-# Xcode
-mas install 497799835
-# Wunderlist: To-Do List & Tasks
-mas install 410628904
 # Transmit
 mas install 403388562
+# Wunderlist: To-Do List & Tasks
+mas install 410628904
+# Xcode
+mas install 497799835
